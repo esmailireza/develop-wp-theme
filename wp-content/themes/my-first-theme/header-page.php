ead3186e80cb8c0c930e80e3fd22ee2d983ff911
esmailireza <esmailireza20@gmail.com>
2024-02-26 14:56:58 +0330

    create custome header

---
+++ wp-content/themes/my-first-theme/header-page.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title of the document</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/page-style.css' ?>">
+</head>
+</html>
--- wp-content/themes/my-first-theme/page.php
+++ wp-content/themes/my-first-theme/page.php
@@ -1,1 +1,2 @@
-<?php
+<?php get_header(name: 'page')?>
+<h2>this is page file!</h2>
