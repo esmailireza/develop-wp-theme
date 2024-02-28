227bc9e8a2c4a49a44b73363902ac6b34d5e3c36
esmailireza <esmailireza20@gmail.com>
2024-02-28 10:12:40 +0330

    project modularization

--- wp-content/themes/my-first-theme/front-page.php
+++ wp-content/themes/my-first-theme/front-page.php
@@ -1,5 +1,11 @@
 <?php get_header(name: '',args: '') ?>
 <body>
 <h2>front-page page</h2>
+<?php get_template_part(slug: 'partials/index',name: 'comments') ?>
+<?php get_template_part(slug: 'partials/index',name: 'content') ?>
+<?php get_template_part(slug: 'partials/index',name: 'course') ?>
+<?php get_template_part(slug: 'partials/index',name: 'slider') ?>
+<?php get_template_part(slug: 'partials/index',name: 'vip') ?>
+
 </body>
 <?php get_footer(name: '',args: '') ?>
---
+++ wp-content/themes/my-first-theme/partials/index-content.php
@@ -0,0 +1,1 @@
+<section>vip section</section>
