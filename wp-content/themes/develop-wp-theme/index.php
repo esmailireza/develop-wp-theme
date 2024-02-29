f00374710fd0cf365256df943702d38a7f238b8f
esmailireza <esmailireza20@gmail.com>
2024-02-29 08:59:43 +0330

    implement ui course-slider section

--- wp-content/themes/develop-wp-theme/index.php
+++ wp-content/themes/develop-wp-theme/index.php
@@ -2,4 +2,5 @@
 <?php get_template_part('partials/nav/nav','nav'); ?>
 <?php get_template_part('partials/index/hero-header','hero-header'); ?>
 <?php get_template_part('partials/index/trip-facts','trip-facts'); ?>
+<?php get_template_part('partials/index/course-slider','course-slider'); ?>
 <?php get_footer(); ?>
