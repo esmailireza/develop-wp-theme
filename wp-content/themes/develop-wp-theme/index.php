acd8f24b88cbc21426337aa6c3d714fca52bbd8a
esmailireza <esmailireza20@gmail.com>
2024-02-29 09:29:21 +0330

    implement ui post section

--- wp-content/themes/develop-wp-theme/index.php
+++ wp-content/themes/develop-wp-theme/index.php
@@ -3,4 +3,5 @@
 <?php get_template_part('partials/index/hero-header','hero-header'); ?>
 <?php get_template_part('partials/index/trip-facts','trip-facts'); ?>
 <?php get_template_part('partials/index/course-slider','course-slider'); ?>
+<?php get_template_part('partials/index/post','post'); ?>
 <?php get_footer(); ?>
