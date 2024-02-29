87b8fbee2d63d3651bc7415a7c03aecd4760e5af
esmailireza <esmailireza20@gmail.com>
2024-02-29 08:44:07 +0330

    add ui trip-facts section to project

--- wp-content/themes/develop-wp-theme/index.php
+++ wp-content/themes/develop-wp-theme/index.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
 <?php get_template_part('partials/nav/nav','nav'); ?>
 <?php get_template_part('partials/index/hero-header','hero-header'); ?>
+<?php get_template_part('partials/index/trip-facts','trip-facts'); ?>
 <?php get_footer(); ?>
