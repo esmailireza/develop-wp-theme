5f354c1997b7cd4dce246de63374b2b9b342fccf
esmailireza <esmailireza20@gmail.com>
2024-03-01 13:10:10 +0330

    done ui home page

--- wp-content/themes/develop-wp-theme/index.php
+++ wp-content/themes/develop-wp-theme/index.php
@@ -5,5 +5,8 @@
 <?php get_template_part('partials/index/course-slider','course-slider'); ?>
 <?php get_template_part('partials/index/post','post'); ?>
 <?php get_template_part('partials/index/post-tech','post-tech'); ?>
+<?php get_template_part('partials/index/vip','vip'); ?>
+<?php get_template_part('partials/index/feedback','feedback'); ?>
+<?php get_template_part('partials/index/newsletter','newsletter'); ?>
 
 <?php get_footer(); ?>
