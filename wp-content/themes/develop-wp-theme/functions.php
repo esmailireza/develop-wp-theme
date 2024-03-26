ed4ddbe58e6b53b0f40b52831e73d5fb893cac52
esmailireza <esmailireza20@gmail.com>
2024-03-26 17:04:33 +0430

    filter alternative image tag

--- wp-content/themes/develop-wp-theme/functions.php
+++ wp-content/themes/develop-wp-theme/functions.php
@@ -2,3 +2,8 @@
 
 include_once '_inc/theme-setup/theme_setup.php';
 add_theme_support('post-thumbnails');
+
+function remove_img_attr($html){
+    return preg_replace('/(width|height)="\d+"\s/',replacement: "",subject: $html);
+}
+add_filter('post_thumbnail_html','remove_img_attr');
