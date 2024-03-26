0119b7b6faf8aed27c1a418c7075e443c6e69355
esmailireza <esmailireza20@gmail.com>
2024-03-26 13:45:43 +0430

    set posts dynamically

--- wp-content/themes/develop-wp-theme/partials/index/post-tech.php
+++ wp-content/themes/develop-wp-theme/partials/index/post-tech.php
@@ -13,6 +13,8 @@
         <div class="row">
 
             <!-- Single Article -->
+            <?php if (have_posts()): ?>
+            <?php while (have_posts()):the_post(); ?>
             <div class="col-lg-4 col-md-4 col-sm-12">
                 <div class="articles_grid_style">
                     <div class="articles_grid_thumb">
@@ -20,7 +22,7 @@
                     </div>
 
                     <div class="articles_grid_caption">
-                        <h4>چطور زبان انگلیسی را سریع یاد بگیریم؟</h4>
+                        <h4><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h4>
                         <div class="articles_grid_author">
                             <div class="articles_grid_author_img"><img src="<?php echo get_template_directory_uri().'/assets/image//user-1.jpg'?>" class="img-fluid" alt="" /></div>
                             <h4>آدام ویلسون</h4>
@@ -28,40 +30,43 @@
                     </div>
                 </div>
             </div>
+            <?php endwhile; ?>
+            <?php else: ?>
+            <div class="alert alert-danger">هیج پستی یافت نشد!</div>
+            <?php endif; ?>
+            <!-- Single Article -->
+<!--            <div class="col-lg-4 col-md-4 col-sm-12">-->
+<!--                <div class="articles_grid_style">-->
+<!--                    <div class="articles_grid_thumb">-->
+<!--                        <a href="blog-detail.html"><img src="--><?php //echo get_template_directory_uri().'/assets/image//b-2.jpg'?><!--" class="img-fluid" alt="" /></a>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="articles_grid_caption">-->
+<!--                        <h4>لزوم یادگیری زبان در دنیای امروزی!</h4>-->
+<!--                        <div class="articles_grid_author">-->
+<!--                            <div class="articles_grid_author_img"><img src="--><?php //echo get_template_directory_uri().'/assets/image//user-2.jpg'?><!--" class="img-fluid" alt="" /></div>-->
+<!--                            <h4>جان اسمیت</h4>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
             <!-- Single Article -->
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="articles_grid_style">
-                    <div class="articles_grid_thumb">
-                        <a href="blog-detail.html"><img src="<?php echo get_template_directory_uri().'/assets/image//b-2.jpg'?>" class="img-fluid" alt="" /></a>
-                    </div>
-
-                    <div class="articles_grid_caption">
-                        <h4>لزوم یادگیری زبان در دنیای امروزی!</h4>
-                        <div class="articles_grid_author">
-                            <div class="articles_grid_author_img"><img src="<?php echo get_template_directory_uri().'/assets/image//user-2.jpg'?>" class="img-fluid" alt="" /></div>
-                            <h4>جان اسمیت</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Single Article -->
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="articles_grid_style">
-                    <div class="articles_grid_thumb">
-                        <a href="blog-detail.html"><img src="<?php echo get_template_directory_uri().'/assets/image//b-3.jpg'?>" class="img-fluid" alt="" /></a>
-                    </div>
-
-                    <div class="articles_grid_caption">
-                        <h4>چطور امتحان حضوری موفقی داشته باشیم؟</h4>
-                        <div class="articles_grid_author">
-                            <div class="articles_grid_author_img"><img src="<?php echo get_template_directory_uri().'/assets/image//user-3.jpg'?>" class="img-fluid" alt="" /></div>
-                            <h4>دنیل ویکیونز</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!--            <div class="col-lg-4 col-md-4 col-sm-12">-->
+<!--                <div class="articles_grid_style">-->
+<!--                    <div class="articles_grid_thumb">-->
+<!--                        <a href="blog-detail.html"><img src="--><?php //echo get_template_directory_uri().'/assets/image//b-3.jpg'?><!--" class="img-fluid" alt="" /></a>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="articles_grid_caption">-->
+<!--                        <h4>چطور امتحان حضوری موفقی داشته باشیم؟</h4>-->
+<!--                        <div class="articles_grid_author">-->
+<!--                            <div class="articles_grid_author_img"><img src="--><?php //echo get_template_directory_uri().'/assets/image//user-3.jpg'?><!--" class="img-fluid" alt="" /></div>-->
+<!--                            <h4>دنیل ویکیونز</h4>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
         </div>
     </div>
