a58c3896efe92a4d193ef59d815369a76f3c2a5a
esmailireza <esmailireza20@gmail.com>
2024-03-27 12:21:24 +0430

    use custom Query

--- wp-content/themes/develop-wp-theme/partials/index/post-tech.php
+++ wp-content/themes/develop-wp-theme/partials/index/post-tech.php
@@ -22,7 +22,7 @@
                         if (has_post_thumbnail()){
                             the_post_thumbnail('full',['class' => 'img-fluid','alt'=> get_the_title()]);
                         }else{
-                            echo '<img src="" class="img-fluid" alt="image" >';
+                            echo '<img src="http://localhost/wordpress.exp/wordpress/wp-content/uploads/2024/03/thumb-logo.jpg" class="img-fluid" alt="image" >';
                         }
                         ?>
                     </div>
@@ -39,6 +39,37 @@
             <?php endwhile; ?>
             <?php else: ?>
             <div class="alert alert-danger">هیج پستی یافت نشد!</div>
+            <?php endif; ?>
+            <br>
+            <?php
+            $the_query = new WP_Query(['author'=> get_current_user_id()]);
+            ?>
+            <?php if ($the_query ->have_posts()): ?>
+                <?php while ($the_query->have_posts()):$the_query->the_post(); ?>
+                    <div class="col-lg-4 col-md-4 col-sm-12">
+                        <div class="articles_grid_style">
+                            <div class="articles_grid_thumb">
+                                <?php
+                                if (has_post_thumbnail()){
+                                    the_post_thumbnail('full',['class' => 'img-fluid','alt'=> get_the_title()]);
+                                }else{
+                                    echo '<img src="http://localhost/wordpress.exp/wordpress/wp-content/uploads/2024/03/thumb-logo.jpg" class="img-fluid" alt="image" >';
+                                }
+                                ?>
+                            </div>
+
+                            <div class="articles_grid_caption">
+                                <h4><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h4>
+                                <div class="articles_grid_author">
+                                    <div class="articles_grid_author_img"><img src="<?php echo get_template_directory_uri().'/assets/image/user-1.jpg'?>" class="img-fluid" alt="" /></div>
+                                    <h4>آدام ویلسون</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <div class="alert alert-danger">هیج پستی یافت نشد!</div>
             <?php endif; ?>
             <!-- Single Article -->
 <!--            <div class="col-lg-4 col-md-4 col-sm-12">-->
